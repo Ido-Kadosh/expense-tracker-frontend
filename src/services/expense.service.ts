1cc43187752c232d94232063cef0691fbfc64b9b
Ido Kadosh <1234kadosh@gmail.com>
2024-05-12 23:02:18 +0300

    fix: move images to public folder

--- src/services/expense.service.ts
+++ src/services/expense.service.ts
@@ -2,18 +2,18 @@ import { IExpense, IExpenseFilter } from '../types/expense';
 import { httpService } from './http.service';
 
 const categories = [
-	{ id: 'j6jeCg7JIdVz9sEwEak88Z4m', txt: 'food', imgUrl: '/src/assets/img/food.png' },
-	{ id: 'UfG1SUshHVikQVahgQ63Q3ql', txt: 'social life', imgUrl: '/src/assets/img/social-life.png' },
-	{ id: '2ZwVLKFh0Y9Cd88wqXoPiPZo', txt: 'pets', imgUrl: '/src/assets/img/pets.png' },
-	{ id: 'db5fIo4qjyNo2d9ns2B0Lm7x', txt: 'transport', imgUrl: '/src/assets/img/transport.png' },
-	{ id: 'gvetnHcVd0NqQsb8H2FjwNU9', txt: 'culture', imgUrl: '/src/assets/img/culture.png' },
-	{ id: 'hnHnosLE0yiPqoZICxobMZcZ', txt: 'household', imgUrl: '/src/assets/img/household.png' },
-	{ id: 'JuhtltNGpIS8E87ToBN4r6h5', txt: 'apparel', imgUrl: '/src/assets/img/apparel.png' },
-	{ id: 'KgW7XxU0Yzwmg3Va5UlCAoyx', txt: 'beauty', imgUrl: '/src/assets/img/beauty.png' },
-	{ id: 'Aa3gKZjHQWQU2ck40KOUfSUW', txt: 'health', imgUrl: '/src/assets/img/health.png' },
-	{ id: 'uSSZjPtmnJcZfxeQfDmhH1cC', txt: 'education', imgUrl: '/src/assets/img/education.png' },
-	{ id: 'UAW5y2GY3NvADdDej7NkoLqD', txt: 'gift', imgUrl: '/src/assets/img/gift.png' },
-	{ id: 'FgCR3Hi2jiCqcH6l5HtlzQB5', txt: 'other', imgUrl: '/src/assets/img/other.png' },
+	{ id: 'j6jeCg7JIdVz9sEwEak88Z4m', txt: 'food', imgUrl: '/food.png' },
+	{ id: 'UfG1SUshHVikQVahgQ63Q3ql', txt: 'social life', imgUrl: '/social-life.png' },
+	{ id: '2ZwVLKFh0Y9Cd88wqXoPiPZo', txt: 'pets', imgUrl: '/pets.png' },
+	{ id: 'db5fIo4qjyNo2d9ns2B0Lm7x', txt: 'transport', imgUrl: '/transport.png' },
+	{ id: 'gvetnHcVd0NqQsb8H2FjwNU9', txt: 'culture', imgUrl: '/culture.png' },
+	{ id: 'hnHnosLE0yiPqoZICxobMZcZ', txt: 'household', imgUrl: '/household.png' },
+	{ id: 'JuhtltNGpIS8E87ToBN4r6h5', txt: 'apparel', imgUrl: '/apparel.png' },
+	{ id: 'KgW7XxU0Yzwmg3Va5UlCAoyx', txt: 'beauty', imgUrl: '/beauty.png' },
+	{ id: 'Aa3gKZjHQWQU2ck40KOUfSUW', txt: 'health', imgUrl: '/health.png' },
+	{ id: 'uSSZjPtmnJcZfxeQfDmhH1cC', txt: 'education', imgUrl: '/education.png' },
+	{ id: 'UAW5y2GY3NvADdDej7NkoLqD', txt: 'gift', imgUrl: '/gift.png' },
+	{ id: 'FgCR3Hi2jiCqcH6l5HtlzQB5', txt: 'other', imgUrl: '/other.png' },
 ];
 
 const BASE_URL = 'expense/';
